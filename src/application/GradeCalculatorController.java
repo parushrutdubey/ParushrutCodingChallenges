bd28ac90333adcba07226c12debd15221911513f
ParushrutDubey <parushrut.dubey@ucalgary.ca>
2023-02-13 23:50:22 +0000

    Grade Calculator after event handling was added.

---
+++ src/application/GradeCalculatorController.java
@@ -0,0 +1,58 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class GradeCalculatorController {
+
+    @FXML
+    private TextField projectTextField;
+
+    @FXML
+    private ChoiceBox<Integer> optionalCoding;
+
+    @FXML
+    private ChoiceBox<Integer> compulsoryCoding;
+
+    @FXML
+    private Slider quizSlider;
+
+    @FXML
+    private Label courseGradeLabel;
+    @FXML
+    void calculateGrade(ActionEvent event) {
+    	double coursegrade;
+    	
+    	String projectGrade= projectTextField.getText();
+    	coursegrade=Double.parseDouble(projectGrade)*0.5;
+    	System.out.print("Project Grade "+projectGrade);
+    	System.out.println(" Course Grade so far "+coursegrade);
+    	
+    	int compulsory= compulsoryCoding.getValue();
+    	double compweight=compulsory*1.25;//There are a total of 20 coding challenges for a weight of 25%. so each will be worth 1.25%
+    	coursegrade= compweight+coursegrade;
+    	System.out.print("Compulsory Coding Challenges passed "+compulsory);
+    	System.out.println(" Course Grade so far "+coursegrade);
+    	
+    	int notcomp=optionalCoding.getValue();
+    	double optionweight=notcomp*(1.25);
+    	coursegrade=optionweight+coursegrade;
+    	System.out.print("Optional Coding Challenges passed "+notcomp);
+    	System.out.println(" Course Grade so far "+coursegrade);
+    	
+    			
+    	double quizGrade= quizSlider.getValue();
+    	double quizweight=quizGrade*2.5;
+    	coursegrade=quizweight+coursegrade;
+    	System.out.println("Quiz Grade "+quizGrade+" Course Grade so far "+coursegrade);
+
+    	courseGradeLabel.setText("Your course grade is "+coursegrade);
+    	//System.out.println("Button was clicked");
+
+    }
+
+}
---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+	
+import java.io.FileInputStream;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+import javafx.scene.layout.VBox;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			FXMLLoader loader=new FXMLLoader();
+			VBox root= loader.load(new FileInputStream("src/application/GradeCalculatorView.fxml"));
+			Scene scene = new Scene(root,500,500);
+			
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Parushrut's Grade Calculator");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}

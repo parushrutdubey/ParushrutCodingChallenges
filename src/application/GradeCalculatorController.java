f3ae855f577799181b7fdb1afedcd44727e50267
ParushrutDubey <parushrut.dubey@ucalgary.ca>
2023-02-14 00:02:48 +0000

    Updated Calculation of course grade based on current semester calculation

--- src/application/GradeCalculatorController.java
+++ src/application/GradeCalculatorController.java
@@ -28,25 +28,25 @@ public class GradeCalculatorController {
     	double coursegrade;
     	
     	String projectGrade= projectTextField.getText();
-    	coursegrade=Double.parseDouble(projectGrade)*0.5;
+    	coursegrade=Double.parseDouble(projectGrade)*0.4;
     	System.out.print("Project Grade "+projectGrade);
     	System.out.println(" Course Grade so far "+coursegrade);
     	
     	int compulsory= compulsoryCoding.getValue();
-    	double compweight=compulsory*1.25;//There are a total of 20 coding challenges for a weight of 25%. so each will be worth 1.25%
+    	double compweight=compulsory*1.5;//There are a total of 20 coding challenges for a weight of 25%. so each will be worth 1.25%
     	coursegrade= compweight+coursegrade;
     	System.out.print("Compulsory Coding Challenges passed "+compulsory);
     	System.out.println(" Course Grade so far "+coursegrade);
     	
     	int notcomp=optionalCoding.getValue();
-    	double optionweight=notcomp*(1.25);
+    	double optionweight=notcomp*(1.5);
     	coursegrade=optionweight+coursegrade;
     	System.out.print("Optional Coding Challenges passed "+notcomp);
     	System.out.println(" Course Grade so far "+coursegrade);
     	
     			
     	double quizGrade= quizSlider.getValue();
-    	double quizweight=quizGrade*2.5;
+    	double quizweight=quizGrade*3;
     	coursegrade=quizweight+coursegrade;
     	System.out.println("Quiz Grade "+quizGrade+" Course Grade so far "+coursegrade);
 

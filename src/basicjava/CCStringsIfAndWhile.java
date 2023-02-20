6ebf0206f28a48352531aaa80c4f3699322945ca
ParushrutDubey <parushrut.dubey@ucalgary.ca>
2023-02-20 20:08:26 +0000

    Debugging for the isDigit test is done

---
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -0,0 +1,29 @@
+package basicjava;
+
+public class CCStringsIfAndWhile {
+
+	public static boolean isDigit(char c) {
+		boolean testing=true;
+		if(c=='0'||c=='1'||c=='9')
+			testing=true;
+		else
+			testing=false;
+		/**
+		 * Checks if the entered value is a digit or not.
+		 * @param c a character which holds the value of the character to be tested
+		 * @return Will return a true value if the character is a digit or else it will be false
+		 */
+		return testing;
+	}
+
+	public static int count(String string, String string2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static int smallestDigit(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module GradeCalculator {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires junit;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }

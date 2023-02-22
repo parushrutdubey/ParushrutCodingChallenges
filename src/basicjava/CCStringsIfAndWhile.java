821b5079c3ad25d31186298dab9f2f72cd80ff98
ParushrutDubey <parushrut.dubey@ucalgary.ca>
2023-02-22 01:33:07 +0000

    Fixed the documentation for the isDigit method

--- src/basicjava/CCStringsIfAndWhile.java
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -1,28 +1,27 @@
 package basicjava;
 
 public class CCStringsIfAndWhile {
-
+	/**
+	 * This method checks if the entered value is a digit or not.
+	 * @param c a character which holds the value of the character to be tested
+	 * @return Will return a true value if the character is a digit or else it will be false
+	 */
 	public static boolean isDigit(char c) {
 		boolean testing=true;
 		if(c=='0'||c=='1'||c=='9')
 			testing=true;
 		else
 			testing=false;
-		/**
-		 * Checks if the entered value is a digit or not.
-		 * @param c a character which holds the value of the character to be tested
-		 * @return Will return a true value if the character is a digit or else it will be false
-		 */
 		return testing;
 	}
 
-	public static int count(String string, String string2) {
-		// TODO Auto-generated method stub
+	public static int count(String s, String p) {
+		
+
 		return 0;
 	}
 
-	public static int smallestDigit(int i) {
-		// TODO Auto-generated method stub
+	public static int smallestDigit(int num) {
 		return 0;
 	}
 

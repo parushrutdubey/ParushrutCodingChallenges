5cbde6134285a7ab9445dc29973270c799a5d61c
ParushrutDubey <parushrut.dubey@ucalgary.ca>
2023-03-09 13:12:10 +0000

    indexOfIgnoreCase method is finished along with documentation

--- src/basicjava/CCArrayList.java
+++ src/basicjava/CCArrayList.java
@@ -5,20 +5,36 @@ import java.util.ArrayList;
 
 public class CCArrayList {
 
-	public static int indexOfIgnoreCase(ArrayList<String> strs, String string) {
-		return 0;
+	/**
+	 * This method gives us the position of a String from the ArrayList
+	 * @param strs the Arraylist provided to us
+	 * @param toFind the String to be found in the Arraylist
+	 * @return the position of the string in the Arraylist based on its presence
+	 */
+	public static int indexOfIgnoreCase(ArrayList<String> strs, String toFind) {
+		int i;
+		String ret;
+	    for (i = 0; i < strs.size(); i++) {
+	        ret = strs.get(i);
+	        if (ret.equalsIgnoreCase(toFind)) {
+	            return i;
+	        }
+	    }
+	    return -1;
 	}
 
+    /**
+     * This method is there to add an element to the arraylist at the given index
+     * @param nums is the ArrayList provided to us
+     * @param numToInsert is the Number which has to be inserted in the arraylist
+     * @param insertAtIndex is the position at which the number is to be inserted
+     */
 	public static void insert(ArrayList<Double> nums, double numToInsert, int insertAtIndex) {
-	    if (insertAtIndex < 0 || insertAtIndex > nums.size()) {
-	        // insertAtIndex is not within the valid range of indices
+	    if (insertAtIndex < 0 || insertAtIndex > nums.size()) 
 	        return;
-	    }
-
-	    if (nums.size() == insertAtIndex) {
-	        // if the index to insert is the same as the size of the list, add the number to the end
+	    if (nums.size() == insertAtIndex) 
 	        nums.add(numToInsert);
-	    } else {
+	    else {
 	        double currentNum = nums.get(insertAtIndex);
 	        nums.set(insertAtIndex, numToInsert);
 	        insert(nums, currentNum, insertAtIndex + 1);

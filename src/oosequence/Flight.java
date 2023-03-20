3a8e51004ae04894285991e317071c1224a69be9
ParushrutDubey <parushrut.dubey@ucalgary.ca>
2023-03-20 17:44:08 +0000

    Updated comments to explain what has been done in the program

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -7,7 +7,7 @@ public class Flight {
 	 Date arr;
 
         /**
-         * This the constructor which performs validation and assigns the values to dep and arr
+         * This the constructor which performs validation and assigns the values to dep and arr after performing 2 validations to check if it is null and whether the dep time is before arr time.
          * @param departure is the value of the departure time
          * @param arrival is the value of the arrival time
          */
@@ -15,15 +15,15 @@ public class Flight {
 	        if (departure == null || arrival == null) {
 	            dep = departure;
 	            arr = arrival;
-	        } else if (departure.before(arrival)) {
+	        } else if (departure.before(arrival)) { //elseif is used as it is the only other case which must be tested. If the arrival time is before departure then we don't want it to assign any value, so there is no else statement for assigning values other than those which are null or dep before arr.
 	            dep = departure;
 	            arr = arrival;
-	        } // else, do not set departure and arrival (they remain null)
+	        } 
 	    }
 
 
 	    /**
-	     * This is a copy constructor which assigns values without validation
+	     * This is a copy constructor which assigns values to dep and arr without validation
 	     * @param copy is the instance 
 	     */
 	    public Flight(Flight copy) {
@@ -35,7 +35,7 @@ public class Flight {
 
 
 		/**
-		 * It is a method which calculates the time of the flight based off a series of validation.
+		 * It is a method which calculates the time of the flight based off a series of validation done to check whether the departure and arrival times are right.
 		 * @return The length of duration of the flight.
 		 */
 	    public long length() {

03ccad92de1ca23a8f0b2dc23fdf1ed087a5c09b
ParushrutDubey <parushrut.dubey@ucalgary.ca>
2023-03-30 01:54:42 +0000

    Completed the Encapsulation CC

--- src/oosequence/Itinerary.java
+++ src/oosequence/Itinerary.java
@@ -1,49 +1,64 @@
 package oosequence;
 
 import java.util.ArrayList;
+import java.util.Date;
+
 
 public class Itinerary {
 	private String name;
-    private ArrayList<Flight> flights;
-
-
-	public Itinerary(String name) {
-        this.name = name;
-        this.flights = new ArrayList<Flight>();
-
+	private ArrayList<Flight> flights;
+	public Itinerary(String Name) {
+	   name=Name;
 	}
-
+	public String getName() {
+		return name;
+	}
 	public ArrayList<Flight> getFlights() {
 		return flights;
 	}
-
 	public void addFlight(Flight m) {
-		int index = 0;
-		if (flights.contains(m)) {
-			index=index-1;
-	    }
-         for (int i = 0; i < flights.size(); i++) {
-             if (m.getDeparture().compareTo(flights.get(i).getArrival()) < 0) 
-                 break;
-             index = i + 1;  
-         }
-         flights.add(index, m);
-}
-	
-
-	public long getTotalLayover() {
-		long totalLayover = 0;
-        for (int i = 0; i < flights.size() - 1; i++) {
-            Flight currentFlight = flights.get(i);
-            Flight nextFlight = flights.get(i + 1);
-            totalLayover += nextFlight.getDeparture().getTime() - currentFlight.getArrival().getTime();
-        }
-        return totalLayover / 60000;
+		ArrayList<Flight> temp= new ArrayList<Flight>();
+		if(flights==null)	{	
+			temp.add(m);
+			flights=temp;
+		}
+		if(flights.size()<6&&flights.get(0)!=m) {
+			flights.add(m);
+			for (int i = 0; i < flights.size(); i++) {
+				Date firstDate=flights.get(i).getDeparture();
+				for (int j = flights.size() - 1; j > i; j--) {
+		        	  Date lastDate=flights.get(j).getDeparture();
+		              if (firstDate.after(lastDate)) {
+		                  Flight tmp = flights.get(i);
+		                  flights.set(i,flights.get(j)) ;
+		                  flights.set(j,tmp);
+		              }
+		        }
+		   }	
+		}
 	}
+	public long getTotalLayover() {
+		long layover=0;
+		if(flights==null) return 0;
 
-	public Object getName() {
-		// TODO Auto-generated method stub
-		return name;
+		if(flights.size()>1) {
+			long firstArrival=0;
+			long  nextDeparture=0;
+			for (int i=0; i<flights.size();i++) {
+				firstArrival=flights.get(i).getArrival().getTime();  		
+				//for (int j =i+1;j<flightlist.size();j++) {
+				if(i+1<flights.size()) {	
+					nextDeparture=flights.get(i+1).getDeparture().getTime();		
+				
+					layover = layover+ ((nextDeparture-firstArrival)/60000);
+				}
+			}
+			return layover;	
+				
+		}
+			
+			
+		return 0;
 	}
 
 }

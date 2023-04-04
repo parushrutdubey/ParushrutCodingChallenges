2e526321c4a04532ddad514853cff6e6513f95db
ParushrutDubey <parushrut.dubey@ucalgary.ca>
2023-04-04 10:27:45 +0000

    Passed all tests for new TripComponent class on rechecking

---
+++ src/oosequence/Itinerary.java
@@ -0,0 +1,64 @@
+package oosequence;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class Itinerary {
+	private String name;
+	private ArrayList<Flight> flights;
+	public Itinerary(String Name) {
+	   name=Name;
+	}
+	public String getName() {
+		return name;
+	}
+	public ArrayList<Flight> getFlights() {
+		return flights;
+	}
+	public void addFlight(Flight m) {
+		ArrayList<Flight> temp= new ArrayList<Flight>();
+		if(flights==null)	{	
+			temp.add(m);
+			flights=temp;
+		}
+		if(flights.size()<6&&flights.get(0)!=m) {
+			flights.add(m);
+			for (int i = 0; i < flights.size(); i++) {
+				Date firstDate=flights.get(i).getDeparture();
+				for (int j = flights.size() - 1; j > i; j--) {
+		        	  Date lastDate=flights.get(j).getDeparture();
+		              if (firstDate.after(lastDate)) {
+		                  Flight tmp = flights.get(i);
+		                  flights.set(i,flights.get(j)) ;
+		                  flights.set(j,tmp);
+		              }
+		        }
+		   }	
+		}
+	}
+	public long getTotalLayover() {
+		long layover=0;
+		if(flights==null) return 0;
+
+		if(flights.size()>1) {
+			long firstArrival=0;
+			long  nextDeparture=0;
+			for (int i=0; i<flights.size();i++) {
+				firstArrival=flights.get(i).getArrival().getTime();  		
+				//for (int j =i+1;j<flightlist.size();j++) {
+				if(i+1<flights.size()) {	
+					nextDeparture=flights.get(i+1).getDeparture().getTime();		
+				
+					layover = layover+ ((nextDeparture-firstArrival)/60000);
+				}
+			}
+			return layover;	
+				
+		}
+			
+			
+		return 0;
+	}
+
+}
--- src/oosequence/TripComponent.java
+++ src/oosequence/TripComponent.java
@@ -7,37 +7,35 @@ public class TripComponent {
 	private Date start;
 	private Date end;
 
-	public TripComponent(Date dep, Date arr) {
+	public TripComponent(Date departure, Date arrival) {
 		
-		if (dep == null || arr == null) {
-			start = dep;
-			end = arr;   
+		if (departure == null || arrival == null) {
+			start = departure;
+			end = arrival;   
 		}
 		
-		if(dep != null && arr != null) {
+		if(departure != null && arrival != null) {
 			
-			if(dep.after(arr)||dep.equals(arr)) {
-				start = dep;
+			if(departure.after(arrival)||departure.equals(arrival)) {
+				start = departure;
 				end = null;
 			}
-			else if(dep.before(arr)){
-				start = dep;
-				end = arr;
+			else if(departure.before(arrival)){
+				start = departure;
+				end = arrival;
 			}
 		}
-	}
+
+    }
+	
 	
 	public TripComponent() {
-		// TODO Auto-generated constructor stub
 		Date temp = new Date();
 		start = temp;
-		end=new Date(temp.getTime() + 3600000);
-		
-		
+		end=new Date(temp.getTime() + 3600000);	
 	}
 
 	public TripComponent(TripComponent c) {
-		// TODO Auto-generated constructor stub
 		start=c.start;
 		end=c.end;
 	}
@@ -48,27 +46,30 @@ public class TripComponent {
 	public Date getEnd() {
 		return end;
 	}
+	
 	public void setStart(Date date) {	
-		if(date == null || end == null)start = date;
-
-		else if (date.before(end))start = date;
+		if(date == null || end == null)
+			start = date;
+		else if (date.before(end))
+			start = date;
 	}
+	
 	public void setEnd(Date date) { 
-		if(date == null || start ==null)end = date;
-		
-		else if (start.before(date))end = date;
+		if(date == null || start ==null)
+			end = date;
+		else if(start.before(date))
+			end = date;
 	}
 
 	public long lengthInSeconds() {
-		long diff = 0;
-		if(start==null||end==null) return diff;
-		if(start!=null & end!=null ) {
-			long departTime = start.getTime();
+		long l;
+        if (start!= null && end != null) {
+        	long departTime = start.getTime();
 			long arriveTime = end.getTime();
-			diff = (arriveTime-departTime)/1000;
-			return diff;
-		}		
-		return diff;
+			l = (arriveTime-departTime)/1000;
+        } else {
+            l=0;
+        }
+        return l;
 	}
-
 }
